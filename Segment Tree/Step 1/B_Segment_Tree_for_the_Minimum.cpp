0da7ff7be59bf1f32f9154fa751fafe6771bb485
ApoorvShri <lcb2021008@iiitl.ac.in>
2024-02-21 02:26:02 +0530

    Segment Tree / Step 1 / B. Segment Tree for the Minimum

---
+++ Segment Tree/Step 1/B_Segment_Tree_for_the_Minimum.cpp
@@ -0,0 +1,88 @@
+// Problem - https://codeforces.com/edu/course/2/lesson/4/1/practice/contest/273169/problem/B
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class SegTree {
+public:
+  vector<int> seg;
+
+  SegTree(int n) {
+    seg.resize(4 * n);
+  }
+
+  int op(int &x, int &y) {
+    return min(x, y);
+  }
+
+  void build(int ind, int low, int high, vector<int> &a) {
+    if (low == high) {
+      seg[ind] = a[low];
+      return;
+    }
+
+    int mid = low + (high - low) / 2;
+
+    build(2 * ind + 1, low, mid, a);
+    build(2 * ind + 2, mid + 1, high, a);
+
+    seg[ind] = op(seg[2 * ind + 1], seg[2 * ind + 2]);
+  }
+
+  int query(int ind, int low, int high, vector<int> &a, int l, int r) {
+    if (high<l || low>r) return INT_MAX;
+    if (low >= l && high <= r) return seg[ind];
+
+    int mid = low + (high - low) / 2;
+
+    int left = query(2 * ind + 1, low, mid, a, l, r);
+    int right = query(2 * ind + 2, mid + 1, high, a, l, r);
+
+    return op(left, right);
+  }
+
+  void pointUpdate(int ind, int low, int high, vector<int> &a, int pos, int val) {
+    if (low == high) {
+      seg[ind] = val;
+      return;
+    }
+
+    int mid = low + (high - low) / 2;
+
+    if (pos <= mid) pointUpdate(2 * ind + 1, low, mid, a, pos, val);
+    else pointUpdate(2 * ind + 2, mid + 1, high, a, pos, val);
+
+    seg[ind] = op(seg[2 * ind + 1], seg[2 * ind + 2]);
+  }
+};
+
+int main() {
+  int n, m;
+  cin >> n >> m;
+
+  vector<int> a(n);
+  for (auto &it : a) cin >> it;
+
+  SegTree st(n);
+  st.build(0, 0, n - 1, a);
+
+  while (m--) {
+    int type;
+    cin >> type;
+
+    if (type == 1) {
+      int i, v;
+      cin >> i >> v;
+
+      st.pointUpdate(0, 0, n - 1, a, i, v);
+      a[i] = v;
+    }
+
+    else {
+      int l, r;
+      cin >> l >> r;
+
+      cout << st.query(0, 0, n - 1, a, l, r - 1) << endl;
+    }
+  }
+}
